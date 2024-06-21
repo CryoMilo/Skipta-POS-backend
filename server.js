e003552cabb80265f3ef48bb533bbc88ada28594
CryoMilo <minkojee@gmail.com>
2024-06-21 17:28:39 +0700

    fixed socket integration

--- server.js
+++ server.js
@@ -2,18 +2,50 @@ const express = require("express");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const http = require("http");
+const cors = require("cors");
+const socketIo = require("socket.io");
+
+const app = express();
+
+app.use(
+	cors({
+		origin: "http://localhost:3000", // Front-end origin
+		methods: ["GET", "POST"],
+	})
+);
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+	cors: {
+		origin: "http://localhost:3000", // Front-end origin
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+
+	socket.on("message", (msg) => {
+		console.log("message: " + msg);
+		io.emit("message", msg);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+});
+
+// SERVER
 const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express();
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
 
 app.use("/api/order", require("./routes/orderRoutes"));
-
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.listen(port, () => console.log(`Server started on port ${port}`));
